59352ff653498c6fe5c2fb37b7a3f518cdde6e0c
lephuongmaivtu2-svg <lephuongmaivtu2@gmail.com>
2025-08-27 23:34:19 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-export default function App() {
+// src/App.jsx
+import { useEffect, useState } from "react";
+
+const GAS_URL = "https://script.google.com/macros/s/AKfycbykGQhqNv-wSqAdcnnENI-fbnOmQwK3RkK6gbeQ98H9ZRXwatYIuGU5bNmyVl8csLUEvg/exec"; // replace bằng web app URL thật
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(GAS_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ action: "getInitData" }),
+    })
+      .then(res => res.json())
+      .then(json => setData(json))
+      .catch(err => console.error("Fetch error:", err));
+  }, []);
+
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Hello from My PWA!</h1>
-      <p>You can install this app to your home screen 🚀</p>
+    <div>
+      <h1>My PWA App</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
+
+export default App;
