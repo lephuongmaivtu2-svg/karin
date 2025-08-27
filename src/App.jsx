4ded7ffac0fbd22a29beee1f05c1f98f260c32b0
lephuongmaivtu2-svg <lephuongmaivtu2@gmail.com>
2025-08-27 22:17:16 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Hello from My PWA!</h1>
+      <p>You can install this app to your home screen 🚀</p>
+    </div>
+  );
+}
